9be0fbcb27aba5bb9dd6f046f13dd640aef04a7b
Bruno Arruda <86083291+bruno-arruda-dev@users.noreply.github.com>
2024-04-27 18:35:16 -0300

    Configura o fastify/cors, aberto para qualquer cliente.

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,13 @@ import fastifySwaggerUI from "@fastify/swagger-ui";
 import { serializerCompiler, validatorCompiler, jsonSchemaTransform } from 'fastify-type-provider-zod';
 import { mailShoot } from "./routes/MailShooter";
 import { errorHandler } from "./error-handler";
+import fastifyCors from "@fastify/cors";
 
 const app = fastify();
+
+app.register(fastifyCors, {
+    origin: '*',
+});
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
@@ -22,12 +27,12 @@ app.register(fastifySwagger, {
         }
     },
     transform: jsonSchemaTransform,
-})
+});
 
 app
 .register(fastifySwaggerUI, {
     routePrefix: 'docs',
-})
+});
 
 
 app.register(mailShoot);
@@ -35,7 +40,7 @@ app.register(mailShoot);
 app.setErrorHandler(errorHandler);
 
 app
-.listen({ port: 3333 })
+.listen({ port: 3333, host: '0.0.0.0' })
 .then(() => {
     
         console.log("MailShoot is ready to shoot on port 3333! 😎");
