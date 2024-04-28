39e5a57664c4e196c85c57726bb651697701a111
Bruno Arruda <86083291+bruno-arruda-dev@users.noreply.github.com>
2024-04-27 22:09:59 -0300

    Adiciona link para documentação interativa na rota GET.

--- src/schemas/home-route-schemas.ts
+++ src/schemas/home-route-schemas.ts
@@ -4,7 +4,7 @@ export const responseStatusOkSchema = z.object({
     error: z.boolean().default(false),
     message: z.string().default("Bem vindo a API MailShoot: API rápida para disparo de emails"),
     repo: z.string().default("https://github.com/bruno-arruda-dev/MailShoot"),
-    documentation: z.string().default(""),
+    documentation: z.string().default("https://mail-shoot.vercel.app/docs/static/index.html"),
     github: z.string().default("https://github.com/bruno-arruda-dev"),
     linkedin: z.string().default("https://www.linkedin.com/in/bruno-arruda-dev/"),
 })
